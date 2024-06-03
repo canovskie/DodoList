bc8d1c6fadc49f88f657bb84613037b75a7d6594
Can Kaynar <cannkaynarr@outlook.com>
2024-06-03 20:35:26 +0300

    Initial Commit

---
+++ DodoList/DodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DodoListApp.swift
+//  DodoList
+//
+//  Created by Can on 3.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
